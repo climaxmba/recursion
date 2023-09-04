26eac531dc8ba1b166dccbf5f20fb97852a07282
Climax Mba <climaxium150@gmail.com>
2023-09-04 18:10:56 +0100

    Create mergeSort

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("Merge Sort");
+function mergeSort(arr = [3, 5, 0, 7, 3, 5, 3, 8, 4, 9]) {
+  const low = 0,
+    high = arr.length - 1,
+    mid = parseInt(low + high / 2);
+
+  if (low < high) {
+    mergeSort(JSON.parse(JSON.stringify(arr)).splice(low, mid));
+    mergeSort(JSON.parse(JSON.stringify(arr)).splice(mid + 1, high));
+  }
+}
+
+mergeSort();
