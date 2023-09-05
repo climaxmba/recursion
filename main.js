8c0e262258f801b753c263d0d8846ff0368b90d2
Climax Mba <climaxium150@gmail.com>
2023-09-05 07:06:46 +0100

    Fix condition & change method

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-function mergeSort(arr = [3, 5, 0, 7, 3, 5, 3, 8, 4, 9]) {
+function mergeSort(arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) {
   const low = 0,
     high = arr.length - 1,
     mid = parseInt(low + high / 2);
 
-  if (low < high) {
-    mergeSort(JSON.parse(JSON.stringify(arr)).splice(low, mid));
-    mergeSort(JSON.parse(JSON.stringify(arr)).splice(mid + 1, high));
+  let splitted = []
+  if (low + 1 < high) {
+    splitted.push(...mergeSort(arr.slice(low, mid)));
+    splitted.push(...mergeSort(arr.slice(mid)));
+  } else {
+    return arr;
   }
+
+  // return merged array
+  return splitted;
 }
 
 mergeSort();
