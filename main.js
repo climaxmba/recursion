0e950ffd73467f72c2349499141cf635eb01dfd3
Climax Mba <climaxium150@gmail.com>
2023-09-06 16:48:01 +0100

    Add merge function

--- main.js
+++ main.js
@@ -7,4 +7,28 @@ function mergeSort(arr = [8, 0, 5, 3, 7, 7, 5, 9, 10]) {
   return merge(low, high);
 }
 
+function merge(arr1, arr2) {
+  let i = 0;
+  let j = 0;
+  let result = [];
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return result;
+}
+
 console.log(mergeSort());
